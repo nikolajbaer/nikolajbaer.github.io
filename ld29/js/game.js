dcea74abb4f688dae5b60a6c380ba40e83e19247
Nikolaj Baer <nikolaj.baer@gmail.com>
2014-04-26 08:37:17 -0700

    make stuff to carry, chasing hopping things

--- ld29/js/game.js
+++ ld29/js/game.js
@@ -2,22 +2,46 @@
 
 Crafty.c('Player', {
     init: function() {
+        this._carrying = null;
         this.requires('2D, Canvas, Actor, Fourway, Color, Gravity, Collision')
             .fourway(4)
             .color('rgb(200,200,200)')
-            .gravity('Floor');
+            .gravity('Floor')
+        .bind("EnterFrame", function(d){
+            if(this._carrying){
+                this._carrying.y = this.y;
+                this._carrying.x = this.x;
+            }
+        });
+    }
+    ,
+    carry: function(e){
+        if(this._carrying != null){ return false; }
+        this._carrying = e;
     }
 });
 
-Crafty.c('NPC',{
+Crafty.c('Carryable', {
+    init: function() {
+        this.requires('2D, Canvas, Actor, Collision, Color')
+        .onHit("Player",function(h){
+            h[0].obj.carry(this);
+        });
+    }
+});
+
+Crafty.c('Wall', {
+    init: function() {
+        this.requires('2D, Collision');
+    }
+});
+
+Crafty.c('Chaser',{
     init: function() {
         this.speed = 2;
         this.requires('2D, Canvas, Actor, Color, Gravity, Collision')
             .color('rgb(200,100,50)')
             .gravity('Floor')
-        .bind("EnterFrame",function(d){
-            this.act();
-        })
         .onHit("Player",function(d){
                 this.destroy();
             },
@@ -27,20 +51,61 @@ Crafty.c('NPC',{
     }
 });
 
-Crafty.c('SurfaceDweller', {
+Crafty.c('Jumper', {
     init: function() {
-        this.speed = 2;
-        this.requires('NPC')
+        this._jumpspeed = 2;
+        this._jumpboost = 50;
+        this._boost = 0;
+        this._boostrandom = 0.2;
+
+        this.requires('Actor, Gravity')
+        .bind("EnterFrame", function(d){
+            if(this._boost > this._jumpspeed){
+                this.y -= this._jumpspeed;
+                this._boost -= this._jumpspeed; 
+            }else if(this._boost > 0){
+                this.y -= this._boost;
+                this._boost = 0;
+            }
+        }) 
     }
     ,
-    act: function() {
-            var p = Crafty("Player");  
-            var ds = this.speed;
-            if(p.y <= this.y){
-                this.x += ((p.x-this.x)<0?-1:1) * ds;
-            }else{
-                this.x += Math.random() > 0.5 ? -ds/4 : ds/4;
+    jump: function() {
+        this._boost += this._jumpboost;
+        if(this._boostrandom != 0){
+            this._boost -= this._boost * (Math.random() * this._boostrandom);
+        }
+    }
+});
+
+Crafty.c('SurfaceDweller', {
+    init: function() {
+        // Home in on the player
+        this._target = Crafty("Player");  
+        this._surface = Crafty("TheSurface");
+        this._speed = 2;
+        this._dir = 0;
+
+        this.requires('Chaser, Jumper')
+        .bind("EnterFrame",function(d){
+            this.x += this._dir * this._speed;
+            if(this._boost == 0){ 
+                if(Math.random() > 0.75){
+                    // adjust direction
+                    this.adjust_direction();
+                    // and jump
+                    this.jump();
+                }
             }
+        })
+    }
+    ,
+    adjust_direction: function(){
+        if(this._target.y <= this.y){
+            this._dir = ((this._target.x-this.x)<0?-1:1);
+        }else{
+            this._dir = Math.random() > 0.5 ? -1 : 1;
+        }   
     }
 });
 
@@ -52,18 +117,27 @@ Game = {
     Crafty.background('black');
 
     var w = Crafty.viewport.width;
+    var h = Crafty.viewport.height;
     var f0 = Crafty.viewport.height/2 - 10;
     var f1 = Crafty.viewport.height-20;
 
 
     // Create entities
-    Crafty.e('Player').attr({x:10,y:10,w:25,h:25});
-    Crafty.e('Floor, 2D, Canvas, Color')
+    Crafty.e('Player')
+        .attr({x:10,y:10,w:25,h:25});
+
+    // Floors
+    Crafty.e('Floor, 2D, Canvas, Color, TheSurface')
         .attr({x:0,y:f0,w:Crafty.viewport.width,h:20})
         .color('green');
     Crafty.e('Floor, 2D, Canvas, Color')
         .attr({x:0,y:f1,w:Crafty.viewport.width,h:20})
         .color('brown');
+    // Walls
+    Crafty.e('Wall')  
+        .attr({x:-10,y:0,w:10,h:h});
+    Crafty.e('Wall')  
+        .attr({x:w,y:0,w:10,h:h});
 
     // Add NPCs 
     for(var i=0;i<10;i++){
@@ -71,5 +145,10 @@ Game = {
             .attr({x:Math.random()*(w-20)+10,y:f0-10,w:10,h:10});
     }
 
+    Crafty.e('Carryable')
+        .attr({x:w - 20,y:f0-20,w:15,h:15})
+        .color('red');
   }
 }
+
+
