05546c5377de13b62f9268deaf99df1234df825b
Nikolaj Baer <nikolaj.baer@gmail.com>
2016-12-10 23:00:34 -0800

    updated with look and feel

---
+++ ld37/js/assets.js
@@ -0,0 +1,30 @@
+var assetsObj = {
+    "sprites": {
+        "images/floor_stones1.png": {
+            tile: 64,
+            tileh: 64,
+            map: { floor_tile: [0,0] }
+        },
+        "images/magic_box.png": {
+            tile: 40,
+            tileh: 40,
+            map: { magic_box: [0,0] }
+        },
+        "images/magicka.png": {
+            tile: 64,
+            tileh: 128,
+            map: {  magicka: [1,0] } 
+        },
+        "images/window.png": {
+            tile: 128,
+            tileh: 128,
+            map: { window_x: [0,0] }
+        },
+        "images/bat.png": {
+            tile: 64,
+            tileh: 32,
+            map: { bat: [1,0] }
+        }
+    }
+};
+
--- ld37/js/game.js
+++ ld37/js/game.js
@@ -1,70 +1,120 @@
 
-
-Crafty.c("Wall",{
-    required: "2D, WebGL, Color, Solid",
+Crafty.c("Bat",{
+    required: "2D, WebGL, Motion, bat, SpriteAnimation",
     init: function(){
-        this.color("green");
+        this.reel("flapping",200,[[1,0],[2,0]])
+            .attr({w:64,h:32,life:500})
+            .animate("flapping",-1);
+        this.vx = Math.random() * 100;
+        this.vy = Math.random() * -50;
     },
     events: {
         "EnterFrame": function(){
-            //this.x += (0.5-Math.random()) * 2;
-            //this.y += (0.5-Math.random()) * 2;
+            this.life -= 1;
+            if(this.life == 0){
+                this.destroy();
+            }
         }
-    }
+    }   
 });
 
 Crafty.c("Brick",{
-    required: "2D, WebGL, Color, Solid, Collision",
+    required: "2D, WebGL, Floor, Gravity, Solid, magic_box",
     init: function(){
-        this.color("#00EE00").hit("Wall");
-        this.axis = "x";
-        this.speed = 1;
+        this.life = 100;
     },
     events: {
         "EnterFrame": function(){
-            this[this.axis] += this.speed; 
-            if(this.hit("Wall")){
-                this.speed *= -1;
+            this.life -= 1;
+            if(this.life == 0){
+                this.destroy();
             }
         }
-    }
+    }   
 });
 
-function make_room(x,y,w,h,wall){
-    var room = {w:w-20,h:h-20,wall:10};
+Crafty.c("Floor",{
+    required: "2D, WebGL, Color"
+});
 
-    Crafty.e('Wall, North')
-        .attr({x:x, y:y, w:room.w,h:room.wall,color:"green"}); // N
-    Crafty.e('Wall, East')
-        .attr({x:x+room.w-room.wall,y:y+room.wall,w:room.wall,h:room.h-room.wall*2,color:"green"}); // E
-    Crafty.e('Wall, South')
-        .attr({x:x,y:y+room.h-room.wall,w:room.w,h:room.wall,color:"green"}); // S
-    Crafty.e('Wall, West')
-        .attr({x:x,y:y+room.wall,w:room.wall,h:room.h-room.wall*2,color:"green"}); // W
-}
+Crafty.c("Wall",{
+    required: "2D, WebGL, Color, Solid"
+});
+
 
 function run(){
-    var w = 800 ;
-    var h = 600;
-
-    Crafty.init(w,h, document.getElementById('game'));
-
-    Crafty.e('2D, WebGL, Color, Fourway, Collision,Particles')
-        .attr({x: 15, y: h/2-15, w:50, h: 50})
-        .color('#FF0000')
-        .fourway(200)
+    var player = Crafty.e('2D, WebGL, magicka, Twoway, Collision, Particles, Gravity, Player, Mouse, SpriteAnimation')
+        .attr({x: 15, y: h/2-15, z:1000, w:64, h: 128, boost:0, antigrav_timer: 0})
+        .twoway(200)
+        .gravity("Floor")
+        .reel("walking",500, [ [1,0],[2,0] ] )
+        .reel("cast",1000, [[3,0],[3,0],[1,0] ] )
+        .reel("standing",1000, [[1,0]] )
+        .bind("EnterFrame",function(evt){
+        })
+        .bind("NewDirection", function(data){
+            if(Math.abs(data.x) > 0){
+                this.animate('walking',-1);
+            }else{
+                this.animate("standing",-1);
+            }
+        })
         .bind('Moved', function(evt){
-            if(this.hit("Solid")){
+            if(this.hit("Solid") && evt.axis == "x"){
                 this[evt.axis] = evt.oldValue;
             }
          })
-        //.particles({maxParticles:150,size:3,sizeRandom:3,speed:2,speedRandom:1.2,lifeSpan:30,angle:30});
+        .bind("KeyDown", function(e){
+            if(e.key == Crafty.keys.SPACE ){
+                this.animate("cast",1);
+                Crafty.e("Bat")
+                        .attr({x:this.x+50,y:this.y});
+            } 
+        })
+        .particles({
+            maxParticles:150,   
+            size:5,
+            sizeRandom:5,
+            speed:2,
+            speedRandom:1.2,
+            lifeSpan:30,
+            startColour:[18,96,255,1],
+            endColour: [33,220,213,0],
+            originOffset: {x:57,y:15}
+        });
 
-    make_room(0,0,w,h,10);
-    for(var i=0;i<5; i++){
-        Crafty.e("Brick")
-            .attr({x:100+(i*100),y:60, h:200, w:20, speed:i+1, axis:"y"});
-    }
+//    Crafty.e("2D, WebGL, Mouse, Background")
+//        .attr({x:0,y:0,w:w,h:h})
+//        .bind("Click", function(e){
+//            console.log("clicked",e);
+//            Crafty.e("Brick")
+//                .attr({x:e.realX,y:e.realY,w:40,h:40})
+//                .gravity("Floor"); 
+//    });
+
+    // Casting Room
+    Crafty.e("Floor")
+        .attr({x:0,y:h-10,w:w,h:10})
+        .color("#ffffff");
+    
+    Crafty.e("Wall")
+        .attr({x:0,y:0,w:10,h:h})
+        .color("#ffffff");
+    Crafty.e("Wall")
+        .attr({x:w-10,y:0,w:10,h:h})
+        .color("#ffffff");
+
+    Crafty.e("2D, WebGL, WindowL, window_x")
+        .attr({x:200,y:200,w:128,h:128}); 
+    Crafty.e("2D, WebGL, WindowR, window_x")
+        .attr({x:600-128,y:200,w:128,h:128}); 
+
 }
 
-run();
+var w = 800 ;
+var h = 600;
+
+Crafty.init(w,h, document.getElementById('game'));
+Crafty.background('#000 url(images/floor_stones1.png) repeat');
+Crafty.load(assetsObj, run)
+
