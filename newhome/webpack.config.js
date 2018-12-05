6b57c5b657beb46939ec4eae2faab50d162f8e79
Nikolaj Baer <nikolaj.baer@gmail.com>
2018-12-05 10:17:23 -0800

    added new home stuff

---
+++ newhome/webpack.config.js
@@ -0,0 +1,47 @@
+function getStyleUse(bundleFilename) {
+  return [
+    {
+      loader: 'file-loader',
+      options: {
+        name: bundleFilename,
+      },
+    },
+    { loader: 'extract-loader' },
+    { loader: 'css-loader' },
+    {
+      loader: 'sass-loader',
+      options: {
+        includePaths: ['./node_modules'],
+      }
+    },
+  ];
+}
+
+module.exports = [
+  {
+    entry: './home.scss',
+    output: {
+      // This is necessary for webpack to compile, but we never reference this js file.
+      filename: 'style-bundle-home.js',
+    },
+    module: {
+      rules: [{
+        test: /home.scss$/,
+        use: getStyleUse('bundle-home.css')
+      }]
+    },
+  },
+  {
+    entry: "./home.js",
+    output: {
+      filename: "bundle-home.js"
+    },
+    module: {
+      loaders: [{
+        test: /home.js$/,
+        loader: 'babel-loader',
+        query: {presets: ['env']}
+      }]
+    },
+  }
+];
