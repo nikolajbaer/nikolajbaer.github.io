9143f1d59043f643999d7feb17507fb9e081e2a1
Nikolaj Baer <nikolaj.baer@gmail.com>
2024-12-18 19:30:41 -0800

    add reload, crash anim, tweak animations and speed

--- toast.js
+++ toast.js
@@ -45,7 +45,7 @@ function initGame(){
     scene.add(skier)
 
     let snowParticles = []
-    const snowParticleGravity = new THREE.Vector3(0,-1,0.2)
+    const snowParticleGravity = new THREE.Vector3(0,-10,0.2)
     const N_PARTICLES = 100
     const snowParticleMesh = new THREE.InstancedMesh(new THREE.SphereGeometry(1,8,4),new THREE.MeshStandardMaterial({color:"white"}),N_PARTICLES)
     snowParticleMesh.frustumCulled = false
@@ -92,19 +92,11 @@ function initGame(){
 
     let gameOver = false
 
-    const handleCrash = (tree) => {
-        gameOver = true
-        const msg = document.createElement('div')
-        msg.className = "message"
-        msg.innerHTML = 'Woops, you crashed! Game Over!'
-        document.body.appendChild(msg)
-    } 
-
     const handleYeti = (tree) => {
         gameOver = true
         const msg = document.createElement('div')
         msg.className = "message"
-        msg.innerHTML = 'Oh no! The Yeti got you!'
+        msg.innerHTML = 'Oh no! The Yeti got you!<br><button onclick="window.location.reload()">reload</button>'
         document.body.appendChild(msg)
     } 
 
@@ -126,7 +118,6 @@ function initGame(){
     scene.add(yeti)
 
     const spawnYeti = () => {
-        console.log("spawning yeti!")
         yeti.rotation.set(0,0,0)
         yeti.visible = true
         yeti.position.set(
@@ -150,14 +141,35 @@ function initGame(){
         currentAction = actions[name]
     }
 
+    const handleCrash = (tree) => {
+        gameOver = true
+        const msg = document.createElement('div')
+        msg.className = "message"
+        msg.innerHTML = 'Woops, you crashed! Game Over!<br><button onclick="window.location.reload()">reload</button>'
+        document.body.appendChild(msg)
+        changeAction("crash",1.0)
+        for(let i=0;i<20;i++){
+            snowParticles.unshift({
+                position: new THREE.Vector3(
+                    skier.position.x + (Math.random()-0.5 * 3),
+                    (Math.random() * 3),
+                    skier.position.z + (Math.random()-0.5),
+                ),
+                velocity:new THREE.Vector3(
+                    velocity.x*(Math.random()-0.5)*2,
+                    Math.random()*0.4+0.2,
+                    Math.random()*velocity.z*0.8,
+                ),
+                scale:Math.random()*0.7+0.1
+            })
+        }
+    } 
+
     const skiTrackPoints = [[],[]]
     let leftSki
     let rightSki
 
     const tick = (delta) => {
-        if(gameOver){
-            return
-        }
         currentSpeed = velocity.length()/4 // completely made up scaling factor
         speedometer.innerHTML = `${currentSpeed.toFixed(0)}mph<br>${distanceTraveled.toFixed(0)}m`
 
@@ -168,61 +180,62 @@ function initGame(){
         skiTrackPoints[0] = skiTrackPoints[0].slice(0,500)
         skiTrackPoints[1] = skiTrackPoints[1].slice(0,500)
 
-        const turnScale = Math.min(currentSpeed,40)/40 * 0.7 + 0.3
-        let turning = 0 
-        if(keys.get('ArrowLeft') || keys.get('MouseLeft')){
-            if(skier.rotation.y > -Math.PI/2){
-                turning = 1 
-                skier.rotation.y -= delta * 3 
-                // we are facing the opposite way
-                // TODO scale weight based on speed
-                changeAction("right_turn",turnScale) 
-            }
-        }else if(keys.get('ArrowRight') || keys.get('MouseRight')){
-            if(skier.rotation.y < Math.PI/2){
-                turning = -1 
-                skier.rotation.y += delta * 3 
-                changeAction("left_turn",turnScale) 
-            }
-        }else{
-            changeAction("skiing",1.0)
-        }
-
-        const vFactor = Math.min(velocity.length(),40)/40
-        if(turning && Math.random() > 1 - (vFactor*0.5 + 0.45)){
-            const vThrow = vFactor * 5 
-            if(turning === 1){
-                snowParticles.unshift({position:leftSkiPos.clone(),velocity:new THREE.Vector3(vThrow*(-2*Math.random()-1),vThrow,0),scale:Math.random()*0.3+(vFactor*0.2)})
-            }else if(turning === -1){
-                snowParticles.unshift({position:rightSkiPos.clone(),velocity:new THREE.Vector3(vThrow*(2*Math.random()+1),vThrow,0),scale:Math.random()*0.3+(vFactor*0.2)})
-            }
-        }
-
-        if(!tucking){
-            if(keys.get('ArrowDown')){
-                console.log("tuck")
-                tucking = true
-                actions.tuck.reset().fadeIn(0.2).play()
-            }
-        }else if(tucking){
-            if(!keys.get('ArrowDown')){
-                console.log("untuck")
-                tucking = false
-                actions.tuck.fadeOut(0.1).stop()
-            }
-            if(turning !== 0 && tucking){
-                actions.tuck.setEffectiveWeight(0.5)
+        // handle input
+        if(!gameOver){
+            const turnScale = Math.min(currentSpeed,20)/20 * 0.7 + 0.3
+            let turning = 0 
+            if(keys.get('ArrowLeft') || keys.get('MouseLeft')){
+                if(skier.rotation.y > -Math.PI/2){
+                    turning = 1 
+                    skier.rotation.y -= delta * 3 
+                    // we are facing the opposite way
+                    // TODO scale weight based on speed
+                    changeAction("right_turn",turnScale) 
+                }
+            }else if(keys.get('ArrowRight') || keys.get('MouseRight')){
+                if(skier.rotation.y < Math.PI/2){
+                    turning = -1 
+                    skier.rotation.y += delta * 3 
+                    changeAction("left_turn",turnScale) 
+                }
             }else{
-                actions.tuck.setEffectiveWeight(1.0)
+                changeAction("skiing",1.0)
             }
-        }
 
-        if(!snowplowing && keys.get('ArrowUp')){
-            snowplowing = true
-            actions.snow_plow.reset().fadeIn(0.2).play()
-        }else if(snowplowing && !keys.get('ArrowUp')){
-            snowplowing = false
-            actions.snow_plow.fadeOut(0.2).stop()
+            const vFactor = Math.min(velocity.length(),40)/40
+            if(turning && Math.random() > 1 - (vFactor*0.5 + 0.45)){
+                const vThrow = vFactor * 5 
+               if(turning === -1){
+                    snowParticles.unshift({position:leftSkiPos.clone(),velocity:new THREE.Vector3(vThrow*(-2*Math.random()-1),vThrow/1000,0),scale:Math.random()*0.3+(vFactor*0.2)})
+                }else if(turning === 1){
+                    snowParticles.unshift({position:rightSkiPos.clone(),velocity:new THREE.Vector3(vThrow*(2*Math.random()+1),vThrow/1000,0),scale:Math.random()*0.3+(vFactor*0.2)})
+                }
+            }
+
+            if(!tucking){
+                if(keys.get('ArrowDown')){
+                    tucking = true
+                    actions.tuck.reset().fadeIn(0.2).play()
+                }
+            }else if(tucking){
+                if(!keys.get('ArrowDown')){
+                    tucking = false
+                    actions.tuck.fadeOut(0.1).stop()
+                }
+                if(turning !== 0 && tucking){
+                    actions.tuck.setEffectiveWeight(0.5)
+                }else{
+                    actions.tuck.setEffectiveWeight(1.0)
+                }
+            }
+
+            if(!snowplowing && keys.get('ArrowUp')){
+                snowplowing = true
+                actions.snow_plow.reset().fadeIn(0.2).play()
+            }else if(snowplowing && !keys.get('ArrowUp')){
+                snowplowing = false
+                actions.snow_plow.fadeOut(0.2).stop()
+            }
         }
 
         // update velocity in the direction of skier
@@ -232,7 +245,7 @@ function initGame(){
         // Downhill friction
         velocity.add(FRICTION.clone().multiplyScalar(delta * Math.sin(skier.rotation.y)))
         // Ski / wind friction
-        velocity.add(velocity.clone().normalize().multiplyScalar(-1).multiplyScalar(frictionScale))
+        velocity.add(velocity.clone().normalize().multiplyScalar(-1).multiplyScalar(gameOver ? 1 : frictionScale))
 
         const dv = velocity.clone().multiplyScalar(delta)
         distanceTraveled += velocity.z * delta
@@ -246,10 +259,10 @@ function initGame(){
         if(yeti.visible){
             const yetiToSkier = new THREE.Vector3().subVectors(skier.position,yeti.position)
             const yetiDist = yetiToSkier.length()
-            if(yetiDist < 1){
+            if(yetiDist < 1 && !gameOver){
                 handleYeti() 
             }else if(yetiDist < 100){
-                const yetiVel = yetiToSkier.normalize().multiplyScalar(velocity.length()*0.6)
+                const yetiVel = yetiToSkier.normalize().multiplyScalar(Math.max(velocity.length()*0.6,15))
                 yeti.position.add(yetiVel.multiplyScalar(delta))
             }
 
@@ -274,11 +287,11 @@ function initGame(){
         for(let i=0;i<snowParticles.length;i++){
             const p = snowParticles[i]
             if(p.position.y > p.scale/2){
-                p.position.add(p.velocity.clone().multiplyScalar(-1*delta))
+                p.position.add(p.velocity.clone().multiplyScalar(delta))
                 p.velocity.add(particleGravity)
             }
         }
-        // snowParticles = snowParticles.slice(0,N_PARTICLES)
+        snowParticles = snowParticles.slice(0,N_PARTICLES)
         for(let i=0;i<N_PARTICLES;i++){
             const p = snowParticles[i]
             if(!p){
@@ -300,11 +313,12 @@ function initGame(){
     scene.add(rightTrack)
     const tracks = [leftTrack,rightTrack]
     const updateTracks = () => {
+        if(gameOver) return
         if(skiTrackPoints[0].length < 2) return
         for(let i=0;i<2;i++){
             const path = new THREE.CatmullRomCurve3(skiTrackPoints[i]) 
             path.closed = false
-            const newGeometry = new THREE.TubeGeometry(path,500,0.3,4,false)
+            const newGeometry = new THREE.TubeGeometry(path,500,0.4,4,false)
             tracks[i].geometry = newGeometry
             trackGeometries[i].dispose()
             trackGeometries[i] = newGeometry
@@ -335,10 +349,12 @@ function initGame(){
     })
     loader.load("toasterbot_skifree.glb",  (gltf) => {
         mixer = new THREE.AnimationMixer( gltf.scene );
-        for(const clipName of ["skiing","left.turn","right.turn","tuck","snow.plow"]){
+        for(const clipName of ["skiing","left.turn","right.turn","tuck","snow.plow","crash"]){
             const clip  = gltf.animations.find((clip) => clip.name === clipName)
             actions[clipName.replace('.','_')] = mixer.clipAction(clip)
         }
+        actions.crash.clampWhenFinished = true
+        actions.crash.loop = THREE.LoopOnce
 
         actions.skiing.play()
         currentAction = actions.skiing
